c21cf7e4d76c398e04f349a21e94e89742cc323b
Eutarides <javifarmacastillo@gmail.com>
2023-04-17 13:53:33 +0200

    javascript advances on buttons

--- git-calculadora/js/number-buttons.js
+++ git-calculadora/js/number-buttons.js
@@ -1,41 +1,46 @@
 export default (() => {
     let buttons = document.querySelectorAll('.button');
     let current = document.querySelector('.current-operation input');
-    let last = current.value[current.value.length-1];
+    let last = current.value[current.value.length - 1];
 
-    function addToOperation(button){
-        if(current.value == 0){
+    function addToOperation(button) {
+        if (current.value == 0) {
             current.value = button.dataset.value;
         }
-        else{
+        else {
             current.value += button.dataset.value;
         }
     };
 
-    function clear(){
+    function clear() {
         current.value = 0;
     };
 
-    function correct(){
+    function correct() {
         current.value -= last;
     }
 
-    function equal(){
-        eval.current.value;
+    function equal() {
+        eval(current.value);
+        console.log("hola");
     }
 
-    buttons.forEach((button)=>{
-        switch(button.dataset.value){
-            case 'C': clear;
-            case 'ER': correct;
-            case '=': equal; 
+    buttons.forEach((button) => {
+        switch (button.dataset.value) {
+            case 'C': clear();
+                break;
+            case 'ER': correct();
+                break;
+            case '=': equal();
+                break;
+            default: button.addEventListener("click", () => {
+                addToOperation(button);
+            });
+                break;
+
         }
-        button.addEventListener("click", () =>{
-            addToOperation(button);
-            console.log(button.dataset.value);
-        });
     })
 
 
-  
+
 })();
