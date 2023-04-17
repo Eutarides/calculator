e88db508ce741e34805730ca8029283f9b4c6adf
Eutarides <javifarmacastillo@gmail.com>
2023-04-17 13:00:17 +0200

    Javascript buttons

--- git-calculadora/js/number-buttons.js
+++ git-calculadora/js/number-buttons.js
@@ -1,52 +1,41 @@
 export default (() => {
-    let button0 = document.querySelector('.zero-button button')
-    button0.addEventListener('click',() =>{
-        console.log(0)
-    } )
+    let buttons = document.querySelectorAll('.button');
+    let current = document.querySelector('.current-operation input');
+    let last = current.value[current.value.length-1];
 
-    let button1 = document.querySelector('.one-button button')
-    button1.addEventListener('click',() =>{
-        console.log(1)
-    } )
+    function addToOperation(button){
+        if(current.value == 0){
+            current.value = button.dataset.value;
+        }
+        else{
+            current.value += button.dataset.value;
+        }
+    };
 
-    let button2 = document.querySelector('.two-button button')
-    button2.addEventListener('click',() =>{
-        console.log(2)
-    } )
+    function clear(){
+        current.value = 0;
+    };
 
-    let button3 = document.querySelector('.three-button button')
-    button3.addEventListener('click',() =>{
-        console.log(3)
-    } )
+    function correct(){
+        current.value -= last;
+    }
 
-    let button4 = document.querySelector('.four-button button')
-    button4.addEventListener('click',() =>{
-        console.log(4)
-    } )
+    function equal(){
+        eval.current.value;
+    }
 
-    let button5 = document.querySelector('.five-button button')
-    button5.addEventListener('click',() =>{
-        console.log(5)
-    } )
+    buttons.forEach((button)=>{
+        switch(button.dataset.value){
+            case 'C': clear;
+            case 'ER': correct;
+            case '=': equal; 
+        }
+        button.addEventListener("click", () =>{
+            addToOperation(button);
+            console.log(button.dataset.value);
+        });
+    })
 
-    let button6 = document.querySelector('.six-button button')
-    button6.addEventListener('click',() =>{
-        console.log(6)
-    } )
 
-    let button7 = document.querySelector('.seven-button button')
-    button7.addEventListener('click',() =>{
-        console.log(7)
-    } )
-
-    let button8 = document.querySelector('.eight-button button')
-    button8.addEventListener('click',() =>{
-        console.log(8)
-    } )
-
-    let button9 = document.querySelector('.nine-button button')
-    button9.addEventListener('click',() =>{
-        console.log(9)
-    } )
   
 })();
